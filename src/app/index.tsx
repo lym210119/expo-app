d1e0e0a7654306212f291b06a89e67ec9614fd91
liyangming <lym210110@gmail.com>
2023-08-13 02:51:19 +0800

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text>Open up App.tsx to start working on your 123app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
